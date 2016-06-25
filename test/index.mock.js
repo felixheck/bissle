780fdde57d4183dd832c757e0c3032122b328b09
Felix Heck <hi@whoTheHeck.de>
2016-06-25 14:49:42 +0200

    add: test mock data

---
+++ test/index.mock.js
@@ -0,0 +1,40 @@
+const mockResponse = [
+  {
+    "_id": "1",
+    "title": "1_foo"
+  },
+  {
+    "_id": "2",
+    "title": "2_foo"
+  },
+  {
+    "_id": "3",
+    "title": "3_foo"
+  },
+  {
+    "_id": "4",
+    "title": "4_foo"
+  },
+  {
+    "_id": "5",
+    "title": "5_foo"
+  },
+  {
+    "_id": "6",
+    "title": "6_foo"
+  },
+  {
+    "_id": "7",
+    "title": "7_foo"
+  },
+  {
+    "_id": "8",
+    "title": "8_foo"
+  },
+  {
+    "_id": "9",
+    "title": "9_foo"
+  },
+];
+
+module.exports = mockResponse;
